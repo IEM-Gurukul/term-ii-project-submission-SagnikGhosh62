ce1ba82b084b7df68944d6320c2ae6b4595c0bb8
SagnikGhosh62 <gsagnik873@gmail.com>
2026-03-26 20:51:04 +0530

    Update PartTimeEmployee.java

--- src/PayrollManager/PartTimeEmployee.java
+++ src/PayrollManager/PartTimeEmployee.java
@@ -5,15 +5,11 @@ public class PartTimeEmployee extends Employee {
     private int hoursWorked;
 
     public PartTimeEmployee(String id, String name, double hourlyRate, int hoursWorked) {
-        // We pass 0 as baseSalary to the super constructor because 
-        // part-time pay is calculated purely by hours.
         super(id, name, 0); 
         this.hourlyRate = hourlyRate;
         this.hoursWorked = hoursWorked;
     }
 
-    // Polymorphism in action: This implementation is different 
-    // from FullTimeEmployee's calculation.
     @Override
     public double calculateSalary() {
         return hourlyRate * hoursWorked;
